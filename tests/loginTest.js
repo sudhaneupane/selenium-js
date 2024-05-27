c283a9a8671cbca5444017a79e23bb6607abda6c
Sudha <sudaneupane.11@gmail.com>
2024-05-27 16:37:26 +0545

    login test

---
+++ tests/loginTest.js
@@ -0,0 +1,21 @@
+const { Builder, By, until } = require("selenium-webdriver");
+const assert = require("assert");
+async function loginTest() {
+  let driver = await new Builder().forBrowser("firefox").build();
+  try {
+    await driver.navigate().to("https://www.facebook.com/");
+
+    await driver.findElement(By.id("email")).sendKeys("s@outlook.com");
+    await driver.findElement(By.id("pass")).sendKeys("133456789");
+    await driver.findElement(By.name("login")).click();
+
+    const pageTitle = await driver.getTitle();
+    // assert usign node assertion
+    assert.strictEqual(pageTitle, "Facebook");
+
+    await driver.wait(until.titleIs("Facebook"), 10000);
+  } finally {
+    await driver.quit();
+  }
+}
+loginTest();
