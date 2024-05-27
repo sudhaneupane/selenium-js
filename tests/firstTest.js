d8709e83b283c1100df0de0e64f7d15cdf2d8c32
Sudha <sudaneupane.11@gmail.com>
2024-05-27 11:48:23 +0545

    first commit

---
+++ tests/firstTest.js
@@ -0,0 +1,27 @@
+const { Builder, By, Key } = require("selenium-webdriver");
+const assert = require("assert");
+
+async function example() {
+  //launch the browser
+  let driver = await new Builder().forBrowser("firefox").build();
+
+  //navigate to our application
+  await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+  //add a todo
+  await driver
+    .findElement(By.id("sampletodotext"))
+    .sendKeys("Learn Selenium", Key.RETURN);
+
+  //assert
+  let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+    return value
+  })
+
+  assert.strictEqual(todoText,"Learn Selenium")
+
+  //close the browser
+    // await driver.quit();
+}
+
+example();
